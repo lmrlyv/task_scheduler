472c2cfca7c7d113a60800241b7cdcac96943e57
Elmar Aliyev <elmar@aliyevs.com>
2024-12-16 23:40:10 +0100

    Improve service definitions by enabling data persistence for db and rabbitmq

---
+++ init_scripts/init_db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS task_scheduler;
+
+CREATE USER IF NOT EXISTS 'test_user'@'%' IDENTIFIED BY 'test_password';
+
+GRANT ALL PRIVILEGES ON task_scheduler.* TO 'test_user'@'%';
+
+ALTER USER 'root'@'%' IDENTIFIED BY 'test';
+
+FLUSH PRIVILEGES;
