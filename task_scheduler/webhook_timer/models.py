3c4f7ac18817a8d23b76464686cde6b3be06a1ee
Elmar Aliyev <elmar@aliyevs.com>
2024-12-16 23:44:00 +0100

    Define WebhookTimer model

--- task_scheduler/webhook_timer/models.py
+++ task_scheduler/webhook_timer/models.py
@@ -1,4 +1,10 @@
+import uuid
+
 from django.db import models
 
 
-# Create your models here.
+class WebhookTimer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    expires_at = models.DateTimeField()
+    url = models.URLField()
+    is_url_called = models.BooleanField(default=False)
