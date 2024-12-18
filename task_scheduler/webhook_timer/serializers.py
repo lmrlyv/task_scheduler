52fdc46af97f68881168258430357392ff5603ba
Elmar Aliyev <elmar@aliyevs.com>
2024-12-18 01:43:34 +0100

    Add docstring for the SetTimerSerializer class

--- task_scheduler/webhook_timer/serializers.py
+++ task_scheduler/webhook_timer/serializers.py
@@ -2,6 +2,22 @@ from rest_framework.serializers import IntegerField, Serializer, URLField, Valid
 
 
 class SetTimerSerializer(Serializer):
+    """Input serializer the set_timer endpoint.
+
+    This serializer validates the data required to set a webhook timer, including the
+    hours, minutes, seconds, and the URL. It ensures that the time is a positive value,
+    the total timer duration is greater than zero, and given url is valid.
+
+    Attributes:
+        hours (IntegerField): The number of hours for the timer. Must be a non-negative integer.
+        minutes (IntegerField): The number of minutes for the timer. Must be a non-negative integer.
+        seconds (IntegerField): The number of seconds for the timer. Must be a non-negative integer.
+        url (URLField): The URL to which the webhook will be sent when the timer expires.
+
+    Overloading Methods:
+        validate(data): Custom validation to ensure the total timer duration is greater than 0 seconds.
+    """
+
     hours = IntegerField(min_value=0, required=True)
     minutes = IntegerField(min_value=0, required=True)
     seconds = IntegerField(min_value=0, required=True)
