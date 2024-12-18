06f2e4df604475812df6d9ab4cb9c3431148341d
Elmar Aliyev <elmar@aliyevs.com>
2024-12-18 03:58:57 +0100

    Grant db user permission to create test database

--- init_scripts/init_db.sql
+++ init_scripts/init_db.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS task_scheduler;
 CREATE USER IF NOT EXISTS 'test_user'@'%' IDENTIFIED BY 'test_password';
 
 GRANT ALL PRIVILEGES ON task_scheduler.* TO 'test_user'@'%';
+GRANT ALL PRIVILEGES ON test_task_scheduler.* TO 'test_user'@'%';
 
 ALTER USER 'root'@'%' IDENTIFIED BY 'test';
 
