59dccbf9dc4b52d0a0ebdf7e865dcada214d6a78
Elmar Aliyev <elmar@aliyevs.com>
2024-12-18 06:12:13 +0100

    Add tests for the start_timer celery task

---
+++ task_scheduler/webhook_timer/tests/test_start_timer.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from unittest.mock import MagicMock, patch
+from uuid import uuid4
+
+from celery.exceptions import Retry
+from django.test import TestCase
+
+from task_scheduler.webhook_timer.models import WebhookTimer
+from task_scheduler.webhook_timer.tasks import start_timer
+
+
+class CeleryTaskStartTimerTests(TestCase):
+
+    @patch("task_scheduler.webhook_timer.tasks.requests.post")
+    @patch("celery.app.task.Context")
+    def test_start_timer_success(
+        self, mock_celery_context: MagicMock, mock_requests_post: MagicMock
+    ):
+        """Test start_timer task successfully triggers webhook and updates db."""
+        timer_id = uuid4()
+
+        # Mock the request.id property of the celery task object
+        mock_celery_request = MagicMock()
+        mock_celery_request.id = timer_id
+        mock_celery_context.return_value = mock_celery_request
+
+        # Mock the requests.post method to return ok response
+        mock_http_response = MagicMock()
+        mock_http_response.ok = True
+        mock_requests_post.return_value = mock_http_response
+
+        WebhookTimer.objects.create(
+            id=timer_id, url="https://example.com/webhook", expires_at=datetime.now()
+        )
+
+        # Assert if the task entry in the database gets updated after executing start_timer task
+        start_timer()
+        self.assertEqual(mock_requests_post.call_count, 1)
+
+        webhook_timer = WebhookTimer.objects.get(id=timer_id)
+        self.assertTrue(webhook_timer.is_url_called)
+
+    @patch("task_scheduler.webhook_timer.tasks.start_timer.retry")
+    @patch("task_scheduler.webhook_timer.tasks.requests.post")
+    @patch("celery.app.task.Context")
+    def test_start_timer_retry(
+        self,
+        mock_celery_context: MagicMock,
+        mock_requests_post: MagicMock,
+        mock_start_timer_retry: MagicMock,
+    ):
+        """Test start_timer task retries if triggering webhook fails."""
+        timer_id = uuid4()
+
+        # Mock the request.id property of the celery task object
+        mock_celery_request = MagicMock()
+        mock_celery_request.id = timer_id
+        mock_celery_context.return_value = mock_celery_request
+
+        # Mock the requests.post method to retrun not ok response
+        mock_http_response = MagicMock()
+        mock_http_response.ok = False
+        mock_requests_post.return_value = mock_http_response
+
+        # Mock the retry method of task to raise Retry exception when called
+        mock_start_timer_retry.side_effect = Retry()
+
+        WebhookTimer.objects.create(
+            id=timer_id, url="https://example.com/webhook", expires_at=datetime.now()
+        )
+
+        # Assert if the task gets retried after triggering webhook fails
+        with self.assertRaises(Retry):
+            start_timer()
